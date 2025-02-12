7ce48c9980d98edb72de086c6a61ea18f0092704
Hasun Nilupul <hasunnilupul16@gmail.com>
2025-02-13 03:24:26 +0530

    added react-use-callback-prompt

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useMemo, useState } from 'react'
-import { Switch } from '@headlessui/react'
+import { useEffect, useMemo, useState } from "react";
+import { Switch } from "@headlessui/react";
+import { useCallbackPrompt } from "react-use-callback-prompt";
 
-import { classNames } from 'utils/helpers'
-import useCallbackPrompt from 'hooks/useCallbackPrompt';
+import { classNames } from "utils/helpers";
 
 const Contact = () => {
   const [form, setForm] = useState({
@@ -10,34 +10,49 @@ const Contact = () => {
     lastName: "",
     email: "",
     message: "",
-    agreed: false
+    agreed: false,
   }); // form state
   const haveUnsavedChanges = useMemo(() => {
-    return form.firstName !== "" || form.lastName !== "" || form.email !== "" || form.message !== "" || form.agreed;
+    return (
+      form.firstName !== "" ||
+      form.lastName !== "" ||
+      form.email !== "" ||
+      form.message !== "" ||
+      form.agreed
+    );
   }, [form]);
-  const [showPrompt, confirmNavigation, cancelNavigation] = useCallbackPrompt(haveUnsavedChanges);
+  const [showPrompt, confirmNavigation, cancelNavigation] =
+    useCallbackPrompt(haveUnsavedChanges);
 
   // handle form fields on change event
   const handleOnChange = (event) => {
-    setForm(prevState => ({ ...prevState, [event.target.name]: event.target.value }));
-  }
+    setForm((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
 
   // submit form values
   const submitForm = () => {
-    localStorage.setItem(`contact${Math.random() * 1000}`, JSON.stringify(form));
+    localStorage.setItem(
+      `contact${Math.random() * 1000}`,
+      JSON.stringify(form)
+    );
     setForm({
       firstName: "",
       lastName: "",
       email: "",
       message: "",
-      agreed: false
+      agreed: false,
     });
     alert("Saved");
-  }
+  };
 
   useEffect(() => {
     if (showPrompt) {
-      const result = window.confirm("There are changes that have not been saved. Save changes now?");
+      const result = window.confirm(
+        "There are changes that have not been saved. Save changes now?"
+      );
       if (result) {
         submitForm();
         confirmNavigation();
@@ -50,7 +65,9 @@ const Contact = () => {
   return (
     <div className="isolate bg-white px-6 py-4 sm:py-2 lg:px-8">
       <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact Us</h2>
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Contact Us
+        </h2>
         <p className="mt-2 text-lg leading-8 text-gray-600">
           Aute magna irure deserunt veniam aliqua magna enim voluptate.
         </p>
@@ -58,7 +75,10 @@ const Contact = () => {
       <form action="#" method="POST" className="mx-auto mt-6 max-w-xl sm:mt-8">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
-            <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+            <label
+              htmlFor="first-name"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
               First name
             </label>
             <div className="mt-2.5">
@@ -74,7 +94,10 @@ const Contact = () => {
             </div>
           </div>
           <div>
-            <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
+            <label
+              htmlFor="last-name"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
               Last name
             </label>
             <div className="mt-2.5">
@@ -90,7 +113,10 @@ const Contact = () => {
             </div>
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+            <label
+              htmlFor="email"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
               Email
             </label>
             <div className="mt-2.5">
@@ -106,7 +132,10 @@ const Contact = () => {
             </div>
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
+            <label
+              htmlFor="message"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
               Message
             </label>
             <div className="mt-2.5">
@@ -117,7 +146,7 @@ const Contact = () => {
                 value={form.message}
                 onChange={handleOnChange}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                defaultValue={''}
+                defaultValue={""}
               />
             </div>
           </div>
@@ -125,24 +154,26 @@ const Contact = () => {
             <div className="flex h-6 items-center">
               <Switch
                 checked={form.agreed}
-                onChange={(checked) => handleOnChange({ target: { name: "agreed", value: checked } })}
+                onChange={(checked) =>
+                  handleOnChange({ target: { name: "agreed", value: checked } })
+                }
                 className={classNames(
-                  form.agreed ? 'bg-indigo-600' : 'bg-gray-200',
-                  'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                  form.agreed ? "bg-indigo-600" : "bg-gray-200",
+                  "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 )}
               >
                 <span className="sr-only">Agree to policies</span>
                 <span
                   aria-hidden="true"
                   className={classNames(
-                    form.agreed ? 'translate-x-3.5' : 'translate-x-0',
-                    'h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out'
+                    form.agreed ? "translate-x-3.5" : "translate-x-0",
+                    "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
                   )}
                 />
               </Switch>
             </div>
             <Switch.Label className="text-sm leading-6 text-gray-600">
-              By selecting this, you agree to our{' '}
+              By selecting this, you agree to our{" "}
               <a href="#" className="font-semibold text-indigo-600">
                 privacy&nbsp;policy
               </a>
@@ -162,6 +193,6 @@ const Contact = () => {
       </form>
     </div>
   );
-}
+};
 
 export default Contact;
